5517c47f866fc931db49bfd3d6fc1e4b91ad9190
MetalNeox <metalneox@gmail.com>
2021-10-18 18:04:47 +0200

    brackets balanced

---
+++ src/utils/string_manipolation.rs
@@ -0,0 +1,14 @@
+//brackets balanced
+pub fn brackets_are_balanced(string: &str) -> bool {
+    let mut parenthesis: Vec<char> = Vec::new();
+    for ch in string.chars() {
+        match ch {
+            '(' | '{' | '[' => parenthesis.push(ch),
+            ')' => if parenthesis.pop() != Some('(') {return false},
+            ']' => if parenthesis.pop() != Some('[') {return false},
+            '}' => if parenthesis.pop() != Some('{') {return false},
+            _ => ()
+        }
+    }
+    parenthesis.is_empty()
+}
