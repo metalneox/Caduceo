462f6698a4918d646a4b8f759dcdc966e4d1fef0
MetalNeox <metalneox@gmail.com>
2021-10-26 22:31:53 +0200

    Add check overflowing collatz function

--- src/math/math.rs
+++ src/math/math.rs
@@ -3,6 +3,12 @@ pub fn collatz(n: u64) -> Option<u64> {
         0 => None,
         1 => Some(0),
         n if n % 2 == 0 => collatz(n/2) .map(|x| x + 1),
-        n => collatz(3 * n + 1).map(|x| x + 1),
-    }
+        n => match n.overflowing_mul(3) {
+            (_,false) => match (n*3).overflowing_add(1){
+                             (_,false) => collatz(3 * n + 1).map(|x| x + 1),
+                             (_,true) => None
+                         }
+            (_,true) => None
+        }
+    } 
 }
