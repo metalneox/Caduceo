7e75fb1a2e49a1c746d329d06c1444e148ad1db8
metalneox <metalneox@gmail.com>
2022-11-09 23:52:23 +0000

    Deploying to gh-pages from @ metalneox/Caduceo@93f076cace2f7e779d3ad5db961ee4c91a541bdb 🚀

--- source-files.js
+++ source-files.js
@@ -1,3 +1,4 @@
-var sourcesIndex = {};
-sourcesIndex["caduceo"] = {"name":"","dirs":[{"name":"crypto","files":["ciphers.rs","mod.rs"]},{"name":"math","files":["fun.rs","mod.rs"]},{"name":"tests","files":["mod.rs","test.rs"]},{"name":"utils","files":["mod.rs","string_manipolation.rs"]}],"files":["lib.rs"]};
+var sourcesIndex = JSON.parse('{\
+"caduceo":["",[["crypto",[],["ciphers.rs","mod.rs"]],["math",[],["fun.rs","mod.rs"]],["tests",[],["mod.rs","test.rs"]],["utils",[],["mod.rs","string_manipolation.rs"]]],["lib.rs"]]\
+}');
 createSourceSidebar();
